f621eee4f7c449d2cbc9678922de1aa25726fbbf
cnc2003 <chanachai.main@gmail.com>
2023-05-12 10:20:03 +0700

    'bankApp1'

---
+++ src/bankingapplication1/Customer.java
@@ -0,0 +1,27 @@
+package bankingapplication1;
+
+public class Customer {
+    private int idCard;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    
+    public Customer(int idCard, String firstName, String lastName, String phoneNumber){
+        this.idCard = idCard;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public int getIdCard(){
+        return this.idCard;
+    }
+    
+    public String getFirstName() {
+        return this.firstName;
+    }
+    
+    public String getLastName() {
+        return this.lastName;
+    }
+}
